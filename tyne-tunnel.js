542bc33c840a72441a001e3eddbc86f28040dc26
Steve Hobbs <elkdanger@googlemail.com>
2018-03-09 21:15:53 +0000

    Updating tyne-tunnel.js

--- tyne-tunnel.js
+++ tyne-tunnel.js
@@ -1,5 +1,5 @@
 const osmosis = require('osmosis');
-const axios = require('axios');
+const request = require('request');
 
 const config = {
   startPage: 'https://www.tt2.co.uk/s/web-user-login.html'
@@ -8,15 +8,12 @@ const config = {
 function sendBalanceUpdate(context, balance, done) {
   const msg = `Your current balance is now ${balance}`;
   
-  axios.post(context.secrets.webhook_url, {
+  console.log(`Sending balance update to webhook: ${msg}`)
+  
+  request.post(context.secrets.webhook_url, {
     json: {
       text: msg
     }
-  }).then(() => {
-    console.log(`Webhook sent with the content: ${msg}`);
-    done(null);
-  }).catch(err => {
-    done(err);
   })
 }
 
@@ -26,48 +23,69 @@ module.exports = function(context, done) {
     email: context.secrets.tt_username,
     password: context.secrets.tt_password
   };
-  
+
   const parseTableField = text => text.substr(text.indexOf(':') + 1).trim()
   
-  osmosis
-    .post(config.startPage, formData)
-    .set({
-      balance: '#balance > strong',
-      name: '.details-left p:first',
-      accountNumber: '.details-right p:eq(4)',
-      customerId: '.details-right p:eq(5)',
-      address: '.details-left p:eq(2)',
-      city: '.details-left p:eq(3)',
-      postcode: '.details-left p:eq(4)',
-      telephone: '.details-left p:eq(5)',
-      email: '.details-left p:eq(6)',
-      lastActivity: '.details-right p:eq(1)',
-      accountType: '.details-right p:eq(3)'
-    })
-    .data((d) => {
-      if (d.balance) {
-        const data = {
-          currentBalance: parseFloat(d.balance.replace(/£([0-9.]+)/, '$1')),
-          name: parseTableField(d.name),
-          accountNumber: parseTableField(d.accountNumber),
-          customerId: parseTableField(d.customerId),
-          email: parseTableField(d.email),
-          lastUpdated: new Date(),
-          address: parseTableField(d.address),
-          city: parseTableField(d.city),
-          postcode: parseTableField(d.postcode),
-          telephone: parseTableField(d.telephone),
-          lastActivity: parseTableField(d.lastActivity),
-          accountType: parseTableField(d.accountType)
+  context.storage.get((err, storageData) => {
+    if (err) {
+      done(err);
+      return
+    }
+      
+    storageData = storageData || { balance: 0 };
+    
+    osmosis
+      .post(config.startPage, formData)
+      .set({
+        balance: '#balance > strong',
+        name: '.details-left p:first',
+        accountNumber: '.details-right p:eq(4)',
+        customerId: '.details-right p:eq(5)',
+        address: '.details-left p:eq(2)',
+        city: '.details-left p:eq(3)',
+        postcode: '.details-left p:eq(4)',
+        telephone: '.details-left p:eq(5)',
+        email: '.details-left p:eq(6)',
+        lastActivity: '.details-right p:eq(1)',
+        accountType: '.details-right p:eq(3)'
+      })
+      .data((d) => {
+        if (d.balance) {
+          const data = {
+            currentBalance: parseFloat(d.balance.replace(/£([0-9.]+)/, '$1')),
+            name: parseTableField(d.name),
+            accountNumber: parseTableField(d.accountNumber),
+            customerId: parseTableField(d.customerId),
+            email: parseTableField(d.email),
+            lastUpdated: new Date(),
+            address: parseTableField(d.address),
+            city: parseTableField(d.city),
+            postcode: parseTableField(d.postcode),
+            telephone: parseTableField(d.telephone),
+            lastActivity: parseTableField(d.lastActivity),
+            accountType: parseTableField(d.accountType)
+          }
+          
+          const result = {
+            balance: data.currentBalance
+          };
+          
+          context.storage.set(result, err => {
+            if (err) {
+              done(err)
+            } else {
+              if (storageData.balance !== result.balance) {
+                sendBalanceUpdate(context, d.balance);
+              }
+              
+              done(null, result);
+            }
+          });
+        } else {
+          done(new Error('Balance update failed'))
         }
-        
-        sendBalanceUpdate(context, d.balance, (err, result) => done(err, { balance: d.balance }))
-
-      } else {
-        console.log(d);
-        done(new Error('Balance update failed'))
-      }
-    })
-    .log(msg => console.log(msg))
-    .error(err => console.error(err))
+      })
+      .log(msg => console.log(msg))
+      .error(err => console.error(err));
+  })
 };
